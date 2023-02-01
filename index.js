73ab79db99453436e16f30fb2eeb071587149256
Anson Guo <anson.guo.921104@gmail.com>
2023-02-01 13:27:32 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+function findMatching(array, driverName) {
+    const nameMatch = names => names.toUpperCase() === driverName.toUpperCase()
+    const newArray = array.filter(nameMatch)
+    return newArray
+}
+
+function fuzzyMatch(array, driverLetter) {
+    const driverLetterLength = driverLetter.length
+    const letterMatch = names => names.slice(0,driverLetterLength).toUpperCase() === driverLetter.toUpperCase()
+    return array.filter(letterMatch)
+}
+
+function matchName(array, driverName) {
+    const nameMatch = obj => obj.name === driverName
+    return array.filter(nameMatch)
+}
